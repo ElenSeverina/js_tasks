73ecdd8584cc86a66c56ae96004a904a9eaf95b9
HelenSeverina <helen.severina23@gmail.com>
2023-02-09 18:36:21 +0200

    getNumberFromString

---
+++ getNumberFromString.js
@@ -0,0 +1,68 @@
+/*
+*   Goal:
+*   Develop a function that returns a number from passed string
+*
+*   Example:
+*   getNumberFromString('s4s 56 s1!@4') -> 45614
+*
+*/
+
+function getNumberFromString(str) {
+  if (typeof str === 'number') {
+    return str;
+  }
+
+  if (typeof str !== 'string') {
+    return null;
+  }
+
+  const arrOfSymbols = str.split('');
+  const arrOfNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let alreadyHasADot = false;
+  let isPreviousSymbolsADot = false;
+  const result = [];
+
+  for (let i = 0; i < arrOfSymbols.length; i++) {
+    const symbol = arrOfSymbols[i];
+
+    if (symbol === '.' && alreadyHasADot) {
+      continue;
+    }
+
+    if (symbol === '.') {
+      isPreviousSymbolsADot = true;
+      continue;
+    }
+
+    if (!arrOfNumbers.includes(symbol)) {
+      isPreviousSymbolsADot = false;
+      continue;
+    }
+
+    if (isPreviousSymbolsADot) {
+      result.push('.');
+    }
+
+    result.push(symbol);
+    isPreviousSymbolsADot = false;
+  }
+
+  return parseFloat(result.join(''));
+}
+
+
+console.assert(getNumberFromString('123') === 123, 1);
+console.assert(getNumberFromString('1234567890') === 1234567890, 2);
+console.assert(getNumberFromString('s4s56s') === 456, 3);
+console.assert(getNumberFromString('s4s 56 s1!@4') === 45614, 4);
+console.assert(getNumberFromString('4.5') === 4.5, 5);
+console.assert(getNumberFromString('.5') === 0.5, 6);
+console.assert(getNumberFromString('5.asd') === 5, 7);
+console.assert(getNumberFromString('as3dd.sda4sd2sad') === 342, 8);
+console.assert(getNumberFromString('as3ddsda.4sd2sad') === 3.42, 9);
+
+console.assert(getNumberFromString(123) === 123, 10);
+console.assert(getNumberFromString(false) === null, 11);
+
+
+console.log('test complited');
