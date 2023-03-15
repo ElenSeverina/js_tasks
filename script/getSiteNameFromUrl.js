35dcad57fc214ca8d90d8c90a1fd4844ad31cdc0
HelenSeverina <helen.severina23@gmail.com>
2023-03-15 17:19:27 +0200

    no commit message

--- script/getSiteNameFromUrl.js
+++ script/getSiteNameFromUrl.js
@@ -6,11 +6,12 @@
     https://subd.sitename.com/ru/post/167015/ -> sitename
 */
 
-const regex = /^(\w+:\/\/)?([^./?]+\.)*([^\\]+)\.+com|ua/;
+const regex = /^(\w+:\/\/)?([a-zA-Z0-9-]+\.)?([a-zA-Z0-9-]+)\.+com|ua[a-zA-Z]{2,}(\/\S*)?$/;
 
 function getSiteNameFromUrl(str) {
   return str.match(regex)[3];
 }
+
 console.assert(getSiteNameFromUrl('https://subd.sitename.com/post/167015/') === 'sitename', 1);
 console.assert(getSiteNameFromUrl('https://tutsplus.com/tutorials/8-regular-expressions-you-should-know') === 'tutsplus', 2);
 console.assert(getSiteNameFromUrl('tutsplus.com/tutorials/8-regular-expressions-you-should-know') === 'tutsplus', 3);
