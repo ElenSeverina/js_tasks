e55a99df927b7a1eb2f7411d79871cd6b873d626
HelenSeverina <helen.severina23@gmail.com>
2023-04-27 18:10:47 +0300

    no commit message

--- Scraper.js
+++ Scraper.js
@@ -1,23 +1,20 @@
 /*
     Goal:
     Develop a Scraper class to use for scrapping text information from pages.
-
     Page 1:
     https://webscraper.io/test-sites/e-commerce/allinone
     Your Goal to scrap information in this csv format:
     product name,prize,stars,reviews,info
-
     Page 2:
     https://webscraper.io/test-sites/tables
     Your Goal to scrap information from all the tables in separate csv files in 2 formats:
     - first name,last name,user name
     - person,user data
 */
-class Scrapper {
-  constructor() {
-  }
-  
+
+class ScrappAllProductsData {
   #products = [];
+  #parsedProducts = [];
 
   #getProductName(item) {
     return item.querySelector(".title").getAttribute("title");
@@ -26,47 +23,107 @@ class Scrapper {
   #getProductPrice(item) {
     return item.querySelector(".price").innerText;
   }
-  
+
   #getProductStars(item) {
     return item.querySelector("p[data-rating]").dataset.rating;
   }
-  
+
   #getProductReview(item) {
     return item.querySelector("p.pull-right").innerText;
   }
-  
+
   #getProductInfo(item) {
     return item.querySelector(".description").innerText;
   }
-  
+
   parse() {
     Array.from(document.querySelectorAll(".thumbnail"))
       .forEach(item => {
         this.#products.push({
-          name: this.#getProductName(item), 
-          price: this.#getProductPrice(item), 
-          stars: this.#getProductStars(item), 
+          name: this.#getProductName(item),
+          price: this.#getProductPrice(item),
+          stars: this.#getProductStars(item),
           reviews: this.#getProductReview(item),
           info: this.#getProductInfo(item)
         });
       });
   }
-  
-  createCSV() {
-    let arr = [`product name\tprice($)\tstars\treviews\tinfo`];
-    this.#products.map(item => {
-      arr.push(`${item.name}\t${item.price.slice(1)}\t${item.stars}\t${+/\d+/.exec(item.reviews)}\t${item.info}`);
-    });
-    return arr.join('\r\n')
+
+  getParsed() {
+    this.#products.forEach(item => Object.entries(item).map(([key, value]) => 
+      this.#parsedProducts.push({
+        header: {
+          title: key,
+          },
+        data: {
+          title: value,
+          }
+      })
+    ))
+    return this.#parsedProducts;
+  }
+
+  fixTitle() {
+    this.#parsedProducts.forEach((_, index) => {
+      if (this.#parsedProducts[index].header.title === 'name') {
+        this.#parsedProducts[index].header.title = 'Product Name';
+      }
+
+      if (this.#parsedProducts[index].header.title === 'price') {
+        this.#parsedProducts[index].header.title = 'Price';
+      }
+
+      if (this.#parsedProducts[index].header.title === 'stars') {
+        this.#parsedProducts[index].header.title = 'Stars';
+      }
+
+      if (this.#parsedProducts[index].header.title === 'reviews') {
+        this.#parsedProducts[index].header.title = 'Reviews';
+      }
+
+      if (this.#parsedProducts[index].header.title === 'info') {
+        this.#parsedProducts[index].header.title = 'Info';
+      }
+    })
+    return this.#parsedProducts;
   }
 }
 
-let newScrapper = new Scrapper();
+class Scrapper {
+  #scrap // TODO rename correctly
+
+  constructor(scrap) {
+    this.#scrap = scrap;
+  }
+
+  parse() {
+    this.#scrap.parse();
+  }
+
+  createCSV() {
+    const parsedData = this.#scrap.getParsed();
+    // let arr = [`product name\tprice($)\tstars\treviews\tinfo`];
+    let header = [`product name\tprice($)\tstars\treviews\tinfo`];
+
+    parsedData.forEach(({ header, data }) => {
+      const c = data.title;
+      header.push[header.title];
+    });
+  }
+
+  createXML() {
+
+  }
+}
+
+let newScrapper = new Scrapper(
+  new ScrappAllProductsData()
+);
 
 newScrapper.parse();
 newScrapper.createCSV();
 
-console.assert(
-  newScrapper.createCSV() === "product name\tprice($)\tstars\treviews\tinfo\r\nLenovo ThinkPad T470\t1349.23\t1\t5\tLenovo ThinkPad T470, 14\" FHD IPS, Core i5-7200U, 8GB, 256GB SSD, Windows 10 Pro\r\nLenovo IdeaPad Miix 510 Platinum Silver\t1212.16\t4\t0\tLenovo IdeaPad Miix 510 Platinum Silver, 12.2\" IPS Touch, Core i5-7200U, 8GB, 256GB SSD, 4G, Windows 10 Pro\r\nMSI GL72M 7RDX\t1099.00\t4\t1\tMSI GL72M 7RDX, 17.3\" FHD, Core i5-7300HQ, 8GB, 1TB + 128GB SSD, GeForce GTX 1050 2GB, Windows 10 Home",
-  'scrap-test',
-);
+// console.assert(
+//   newScrapper.createCSV() === "product name\tprice($)\tstars\treviews\tinfo\r\nLenovo ThinkPad T470\t1349.23\t1\t5\tLenovo ThinkPad T470, 14\" FHD IPS, Core i5-7200U, 8GB, 256GB SSD, Windows 10 Pro\r\nLenovo IdeaPad Miix 510 Platinum Silver\t1212.16\t4\t0\tLenovo IdeaPad Miix 510 Platinum Silver, 12.2\" IPS Touch, Core i5-7200U, 8GB, 256GB SSD, 4G, Windows 10 Pro\r\nMSI GL72M 7RDX\t1099.00\t4\t1\tMSI GL72M 7RDX, 17.3\" FHD, Core i5-7300HQ, 8GB, 1TB + 128GB SSD, GeForce GTX 1050 2GB, Windows 10 Home",
+//   'scrap-test',
+// );
